6a444e271b6f82c994dfb61cc9db7cba4c504f64
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-12-13 19:59:50 +0100

    Repair test

--- src/app/answer/service/answer.service.spec.ts
+++ src/app/answer/service/answer.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AnswerService } from './answer.service';
+import {provideHttpClient} from "@angular/common/http";
 
 describe('AnswerService', () => {
   let service: AnswerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()]
+    });
     service = TestBed.inject(AnswerService);
   });
 
