8d4c8076d85a7b2ef532796ee5c3e17c2eaaa9d9
Michael Lechowicz <ichal.lechowicz@gmail.com>
2024-01-04 19:09:48 +0100

    Add Angular Material for Main Menu style

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    MatMenuModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
